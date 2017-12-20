c5828854e3072af07d48d3da7e2d8b0a3e909e0b
le.van.hai <le.van.hai@miyatsu.vn>
2017-12-20 17:04:38 +0700

    update

--- index.php
+++ index.php
@@ -346,7 +346,7 @@ if(isset($_GET['lang'])) {
                                 <p><img src="/images/bg7/icon2.png" alt=""><?= $block73[$LANG] ?></p>
                             </div>
                             <div class="content">
-                                <p><a class="contact" href="https://admin.botchan.chat:3030/efo?connect_page_id=5a3a292e05940871e066b422"><?= $block731[$LANG] ?></a></p>
+                                <p><a class="contact" href="https://admin.botchan.chat:3030/efo?connect_page_id=5a3a292e05940871e066b422" target="_blank"><?= $block731[$LANG] ?></a></p>
                             </div>
                         </div>
                     </li>
