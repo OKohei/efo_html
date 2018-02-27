73cd657e4c8bf7ebff46c41bc81c8c395e0c6bca
MIYATSU\le.thanh.hai <le.thanh.hai@miyatsu.vn>
2018-02-27 13:18:18 +0700

    fix

--- index.php
+++ index.php
@@ -19,6 +19,15 @@ goog_report_conversion when someone clicks on the
 chosen link or button. -->
 <script type="text/javascript">
     /* <![CDATA[ */
+    var google_conversion_id = 947605038;
+    var google_custom_params = window.google_tag_params;
+    var google_remarketing_only = true;
+    /* ]]> */
+</script>
+
+
+<script type="text/javascript">
+    /* <![CDATA[ */
     goog_snippet_vars = function() {
         var w = window;
         w.google_conversion_id = 947605038;
@@ -42,9 +51,14 @@ chosen link or button. -->
     }
     /* ]]> */
 </script>
-<script type="text/javascript"
-        src="//www.googleadservices.com/pagead/conversion_async.js">
+
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
 </script>
+<noscript>
+    <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/947605038/?guid=ON&amp;script=0"/>
+    </div>
+</noscript>
 
 <?php include 'header.php';?>
 <div id="what">
